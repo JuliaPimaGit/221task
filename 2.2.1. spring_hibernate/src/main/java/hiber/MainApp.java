7e78381e201691f5395255423d6c89a303cf9768
Julia Pimmahova <pimahovayulia@gmail.com>
2022-03-15 14:44:42 +0300

    Added class Car

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,15 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+      Car car1  = new Car("tesla", 3);
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      user1.setCar(car1);
+      userService.add(user1);
+
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -29,6 +35,9 @@ public class MainApp {
          System.out.println();
       }
 
-      context.close();
+      User userByCar = userService.findUserByCar("tesla", 3);
+      System.out.println("The owner of "+ userByCar.getCar().getModel()+" is "+ userByCar.getFirstName()+" "+userByCar.getLastName());
+
+          context.close();
    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -2,6 +2,7 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -11,19 +12,29 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User findUserByCar(String model, int series) {
+        String hql = "FROM User u WHERE u.car.model=:Pm and u.car.series=:Ps";
+        Query query = sessionFactory.getCurrentSession().createQuery(hql);
+        query.setParameter("Ps", series);
+        query.setParameter("Pm", model);
+        return  (User) query.uniqueResult();
+    }
 
 }
